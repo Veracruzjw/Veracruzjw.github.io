06a7e77f3ff40b2e8db52e8f44fc73cc2db23b6c
Veracruzjw <veracruzjw@gmail.com>
2026-02-23 16:54:42 +0100

    first js assignment commit

---
+++ assignment.js
@@ -0,0 +1,8 @@
+// Fruit Basket Calculator
+let fruitBasket = ["Apple", "Banana", "Cantaloupe", "Dates", "Elderberry"];
+
+function countFruits() {
+    console.log("There are " + fruitBasket.length + " fruits in the basket.");
+}
+
+countFruits();
